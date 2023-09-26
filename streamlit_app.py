35f3a43b1adc5bbce85561481783fd7add9de974
sravani-narindi <145702089+sravani-narindi@users.noreply.github.com>
2023-09-27 00:04:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice_1 = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice_1)
+streamlit.write('Thanks for adding', add_my_fruit)
 
-
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice_1)
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
